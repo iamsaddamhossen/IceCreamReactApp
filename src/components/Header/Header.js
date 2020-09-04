70d0c511d5dcf3011b11c5fdc9091c7a60f3ff6f
Saddam Hossen <saddamhossen.cse007@gmail.com>
2020-09-04 17:50:17 +0600

    header file

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+ const Header = () => {
+    return (
+        <header>
+        <div className="container react" id="header">
+            <div>
+                <img class="logo" src="images/logo.svg" alt="logo"/>
+            </div>
+
+            <div class="textRight">
+                <img class="reactLogo" src="images/react.svg" alt="React"/>
+                <strong>React</strong>
+            </div>
+            
+        </div>
+        </header>
+    )
+}
+
+export default Header
